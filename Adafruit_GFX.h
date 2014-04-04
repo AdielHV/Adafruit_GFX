8a0f365df2f32437fed16b577e132d265739946c
Hampus Sandberg <hampus.sandberg@gmail.com>
2014-04-04 21:36:02 +0200

    Changed everything from c++ to c using a struct the represent GFX objects. Compiles without problem

--- Adafruit_GFX.h
+++ Adafruit_GFX.h
@@ -1,87 +1,70 @@
+/* Define to prevent recursive inclusion -------------------------------------*/
 #ifndef _ADAFRUIT_GFX_H
 #define _ADAFRUIT_GFX_H
 
-#if ARDUINO >= 100
- #include "Arduino.h"
- #include "Print.h"
-#else
- #include "WProgram.h"
-#endif
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f10x.h"
 
+/* Defines -------------------------------------------------------------------*/
 #define swap(a, b) { int16_t t = a; a = b; b = t; }
 
-class Adafruit_GFX : public Print {
+/* Typedefs ------------------------------------------------------------------*/
+typedef enum
+{ false = 0,
+  true = !false
+} boolean;
 
- public:
+typedef struct
+{
+	const int16_t WIDTH, HEIGHT;	/* This is the 'raw' display w/h - never changes */
+	int16_t _width, _height;		/* Display w/h as modified by current rotation */
+	int16_t cursor_x, cursor_y;
+	uint16_t textcolor, textbgcolor;
+	uint8_t textsize, rotation;
+	boolean wrap;					/* If set, 'wrap' text at right edge of display */
 
-  Adafruit_GFX(int16_t w, int16_t h); // Constructor
+	void (*drawPixel)(int16_t, int16_t, uint16_t);	/* This MUST be set by the creator of a GFX_Object */
+} GFX_Object;
 
-  // This MUST be defined by the subclass:
-  virtual void drawPixel(int16_t x, int16_t y, uint16_t color) = 0;
+/* Function prototypes -------------------------------------------------------*/
+void Adafruit_GFX_Init(GFX_Object *Object);
 
-  // These MAY be overridden by the subclass to provide device-specific
-  // optimized code.  Otherwise 'generic' versions are used.
-  virtual void
-    drawLine(int16_t x0, int16_t y0, int16_t x1, int16_t y1, uint16_t color),
-    drawFastVLine(int16_t x, int16_t y, int16_t h, uint16_t color),
-    drawFastHLine(int16_t x, int16_t y, int16_t w, uint16_t color),
-    drawRect(int16_t x, int16_t y, int16_t w, int16_t h, uint16_t color),
-    fillRect(int16_t x, int16_t y, int16_t w, int16_t h, uint16_t color),
-    fillScreen(uint16_t color),
-    invertDisplay(boolean i);
+void Adafruit_GFX_drawCircle(GFX_Object *Object, int16_t x0, int16_t y0, int16_t r, uint16_t color);
+void Adafruit_GFX_drawCircleHelper(GFX_Object *Object, int16_t x0, int16_t y0, int16_t r, uint8_t cornername, uint16_t color);
+void Adafruit_GFX_fillCircle(GFX_Object *Object, int16_t x0, int16_t y0, int16_t r, uint16_t color);
+void Adafruit_GFX_fillCircleHelper(GFX_Object *Object, int16_t x0, int16_t y0, int16_t r, uint8_t cornername, int16_t delta, uint16_t color);
 
-  // These exist only with Adafruit_GFX (no subclass overrides)
-  void
-    drawCircle(int16_t x0, int16_t y0, int16_t r, uint16_t color),
-    drawCircleHelper(int16_t x0, int16_t y0, int16_t r, uint8_t cornername,
-      uint16_t color),
-    fillCircle(int16_t x0, int16_t y0, int16_t r, uint16_t color),
-    fillCircleHelper(int16_t x0, int16_t y0, int16_t r, uint8_t cornername,
-      int16_t delta, uint16_t color),
-    drawTriangle(int16_t x0, int16_t y0, int16_t x1, int16_t y1,
-      int16_t x2, int16_t y2, uint16_t color),
-    fillTriangle(int16_t x0, int16_t y0, int16_t x1, int16_t y1,
-      int16_t x2, int16_t y2, uint16_t color),
-    drawRoundRect(int16_t x0, int16_t y0, int16_t w, int16_t h,
-      int16_t radius, uint16_t color),
-    fillRoundRect(int16_t x0, int16_t y0, int16_t w, int16_t h,
-      int16_t radius, uint16_t color),
-    drawBitmap(int16_t x, int16_t y, const uint8_t *bitmap,
-      int16_t w, int16_t h, uint16_t color),
-    drawChar(int16_t x, int16_t y, unsigned char c, uint16_t color,
-      uint16_t bg, uint8_t size),
-    setCursor(int16_t x, int16_t y),
-    setTextColor(uint16_t c),
-    setTextColor(uint16_t c, uint16_t bg),
-    setTextSize(uint8_t s),
-    setTextWrap(boolean w),
-    setRotation(uint8_t r);
+void Adafruit_GFX_drawLine(GFX_Object *Object, int16_t x0, int16_t y0, int16_t x1, int16_t y1, uint16_t color);
 
-#if ARDUINO >= 100
-  virtual size_t write(uint8_t);
-#else
-  virtual void   write(uint8_t);
-#endif
+void Adafruit_GFX_drawRect(GFX_Object *Object, int16_t x, int16_t y, int16_t w, int16_t h, uint16_t color);
 
-  int16_t
-    height(void),
-    width(void);
+void Adafruit_GFX_drawFastVLine(GFX_Object *Object, int16_t x, int16_t y, int16_t h, uint16_t color);
+void Adafruit_GFX_drawFastHLine(GFX_Object *Object, int16_t x, int16_t y, int16_t w, uint16_t color);
 
-  uint8_t getRotation(void);
+void Adafruit_GFX_fillRect(GFX_Object *Object, int16_t x, int16_t y, int16_t w, int16_t h, uint16_t color);
+void Adafruit_GFX_fillScreen(GFX_Object *Object, uint16_t color);
 
- protected:
-  const int16_t
-    WIDTH, HEIGHT;   // This is the 'raw' display w/h - never changes
-  int16_t
-    _width, _height, // Display w/h as modified by current rotation
-    cursor_x, cursor_y;
-  uint16_t
-    textcolor, textbgcolor;
-  uint8_t
-    textsize,
-    rotation;
-  boolean
-    wrap; // If set, 'wrap' text at right edge of display
-};
+void Adafruit_GFX_drawRoundRect(GFX_Object *Object, int16_t x, int16_t y, int16_t w, int16_t h, int16_t r, uint16_t color);
+void Adafruit_GFX_fillRoundRect(GFX_Object *Object, int16_t x, int16_t y, int16_t w, int16_t h, int16_t r, uint16_t color);
 
-#endif // _ADAFRUIT_GFX_H
+void Adafruit_GFX_drawTriangle(GFX_Object *Object, int16_t x0, int16_t y0, int16_t x1, int16_t y1, int16_t x2, int16_t y2, uint16_t color);
+void Adafruit_GFX_fillTriangle(GFX_Object *Object, int16_t x0, int16_t y0, int16_t x1, int16_t y1, int16_t x2, int16_t y2, uint16_t color);
+
+void Adafruit_GFX_write(GFX_Object *Object, uint8_t c);
+void Adafruit_GFX_drawChar(GFX_Object *Object, int16_t x, int16_t y, unsigned char c, uint16_t color, uint16_t bg, uint8_t size);
+
+void Adafruit_GFX_setCursor(GFX_Object *Object, int16_t x, int16_t y);
+void Adafruit_GFX_setTextSize(GFX_Object *Object, uint8_t s);
+void Adafruit_GFX_setTextColorTransparentBg(GFX_Object *Object, uint16_t c);
+void Adafruit_GFX_setTextColor(GFX_Object *Object, uint16_t c, uint16_t b);
+void Adafruit_GFX_setTextWrap(GFX_Object *Object, boolean w);
+
+uint8_t Adafruit_GFX_getRotation(GFX_Object *Object);
+void Adafruit_GFX_setRotation(GFX_Object *Object, uint8_t x);
+
+int16_t Adafruit_GFX_width(GFX_Object *Object);
+int16_t Adafruit_GFX_height(GFX_Object *Object);
+
+void Adafruit_GFX_invertDisplay(GFX_Object *Object, boolean i);
+
+#endif /* _ADAFRUIT_GFX_H */
